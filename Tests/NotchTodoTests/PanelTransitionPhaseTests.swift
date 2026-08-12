c086d13f2b1daf5f23f2067b74cc6e3eaabb581a
zhangshuo12 <zhangshuo12@guazi.com>
2026-08-12 19:38:40 +0800

    Revert "fix: make fish bubbles visible"
    This reverts commit d77cd8c83df034942acb1907dd5bdb859e5e5a5b.

--- Tests/NotchTodoTests/PanelTransitionPhaseTests.swift
+++ Tests/NotchTodoTests/PanelTransitionPhaseTests.swift
@@ -24,10 +24,6 @@ final class PanelTransitionPhaseTests: XCTestCase {
         XCTAssertEqual(PixelFishView.bubbleCooldown, 15, accuracy: 0.001)
     }
 
-    func testFishBubblesStayVisibleForThreeSeconds() {
-        XCTAssertEqual(PixelFishView.bubbleSequenceDuration, 3, accuracy: 0.001)
-    }
-
     func testFishBubblesRespectReduceMotion() {
         XCTAssertTrue(PixelFishView.shouldAnimateBubbles(shouldReduceMotion: false))
         XCTAssertFalse(PixelFishView.shouldAnimateBubbles(shouldReduceMotion: true))
